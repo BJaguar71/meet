b08d9a2c4ac83acd45caa98816031c636157cbd5
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-10-05 16:58:23 +0200

    extracted client_secret, client_id, redirect_uris, calendar_id from credentials to define an authporization for client

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -25,3 +25,10 @@ const credentials = {
   ],
 };
 
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
