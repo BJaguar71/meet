79dd8ed593079e4fa71a4d5dfd46fa23978fcef9
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-10-05 16:56:26 +0200

    defined the credentials

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -7,3 +7,21 @@ const calendar = google.calendar("v3");
  */
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
+// credentials
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: [
+    "https://bjaguar71.github.io/meet/"
+  ],
+  javascript_origins: [
+    "https://bjaguar71.github.io",
+    "https://localhost:3000"
+  ],
+};
+
