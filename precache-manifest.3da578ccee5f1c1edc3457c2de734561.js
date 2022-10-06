dcf64ba24487e3a5f996957681aa601cacce1cce
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-10-06 14:39:06 +0200

    Updates

---
+++ precache-manifest.3da578ccee5f1c1edc3457c2de734561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3195ab9254a78abd16a2",
+    "url": "/meet/static/css/main.5ecab2e9.chunk.css"
+  },
+  {
+    "revision": "3195ab9254a78abd16a2",
+    "url": "/meet/static/js/main.3195ab92.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/meet/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/meet/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "a21e384eb6843b2b897d",
+    "url": "/meet/static/js/runtime~main.a21e384e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/meet/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2c5019c3a89a98a1081bffc504485284",
+    "url": "/meet/index.html"
+  }
+];
